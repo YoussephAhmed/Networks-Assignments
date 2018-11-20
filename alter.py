ad08d1f62e2e6498e927e82eaf910be2f5db6e09
MennaEhab <mennaehab21.5@gmail.com>
2018-11-20 09:37:23 +0200

    alter

---
+++ alter.py
@@ -0,0 +1,19 @@
+def alter( str , index ):
+   
+   index=index-1
+  
+   x = list(str)  
+ 
+ 
+   if x[index]=="0":
+    	x[index]="1"
+   else:
+    x[index]="0"
+	 
+   x = "".join(x)
+   #print (x)
+   return x ;
+
+#x="110111"
+#m=alter (x , 3)
+#print(m)
